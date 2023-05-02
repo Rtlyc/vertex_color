bf70fd6a870e99b41f4500bdc39891759d747851
Yuchen Liu <yuchenliu0513@gmail.com>
2023-05-02 18:40:52 -0400

    format codes

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-//! Constants
+// Constants
 const width = 800;
 const height = 600;
 const radius = 20;
 
-console.log("Greedy Coloring");
-
-//! Graph data
+// Graph data
 const graph = {
-  nodes: [{ id: "A" }, { id: "B" }, { id: "C" }, { id: "D" }, { id: "E" }],
+  nodes: [
+    { id: "A" },
+    { id: "B" },
+    { id: "C" },
+    { id: "D" },
+    { id: "E" },
+  ],
   links: [
     { source: "A", target: "B" },
     { source: "A", target: "C" },
@@ -18,8 +22,8 @@ const graph = {
   ],
 };
 
-//! Greedy coloring algorithm
-const colorGraph = (graph) => {
+// Greedy coloring algorithm
+const colorGraphGreedy = (graph) => {
   const steps = [];
   const colors = {};
 
@@ -47,7 +51,7 @@ const colorGraph = (graph) => {
   return steps;
 };
 
-//! DSatur algorithm
+// DSatur algorithm
 const colorGraphDSatur = (graph) => {
   const steps = [];
   const colors = {};
@@ -103,6 +107,7 @@ const colorGraphDSatur = (graph) => {
   return steps;
 };
 
+// Recursive largest first algorithm
 function colorGraphRLF(graph) {
   const steps = [];
   const colors = {};
@@ -158,18 +163,10 @@ function areNodesAdjacent(node1, node2, graph) {
   );
 }
 
+// Visualization setup
+const svg = d3.select("#visualization").append("svg").attr("width", width).attr("height", height);
 
-//! Visualization setup
-const svg = d3
-  .select("#visualization")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-const colorScale = d3
-  .scaleOrdinal()
-  .domain(graph.nodes.map((node) => node.id))
-  .range(d3.schemeCategory10);
+const colorScale = d3.scaleOrdinal().domain(graph.nodes.map((node) => node.id)).range(d3.schemeCategory10);
 
 const nodeById = new Map(graph.nodes.map((node) => [node.id, node]));
 
@@ -178,49 +175,28 @@ graph.links.forEach((link) => {
   link.target = nodeById.get(link.target);
 });
 
-const simulation = d3
-  .forceSimulation(graph.nodes)
+const simulation = d3.forceSimulation(graph.nodes)
   .force("charge", d3.forceManyBody().strength(-400))
   .force("link", d3.forceLink(graph.links).distance(100))
   .force("center", d3.forceCenter(width / 2, height / 2))
   .on("tick", ticked);
 
-//! Create links
-const link = svg
-  .selectAll(".link")
-  .data(graph.links)
-  .enter()
-  .append("line")
-  .attr("class", "link")
-  .attr("stroke", "black")
-  .attr("stroke-width", 2);
+// Create links
+const link = svg.selectAll(".link").data(graph.links).enter().append("line")
+  .attr("class", "link").attr("stroke", "black").attr("stroke-width", 2);
 
-//! Create nodes
-const node = svg
-  .selectAll(".node")
-  .data(graph.nodes)
-  .enter()
-  .append("circle")
-  .attr("class", "node")
-  .attr("r", radius)
-  .call(drag(simulation));
+// Create nodes
+const node = svg.selectAll(".node").data(graph.nodes).enter().append("circle")
+  .attr("class", "node").attr("r", radius).call(drag(simulation));
 
-//! Create labels
-const label = svg
-  .selectAll(".label")
-  .data(graph.nodes)
-  .enter()
-  .append("text")
-  .attr("class", "label")
-  .text((d) => d.id)
-  .attr("text-anchor", "middle")
-  .attr("dy", ".35em")
-  .style("pointer-events", "none")
-  .style("user-select", "none")
-  .style("font-size", "16px")
-  .style("font-weight", "bold");
+// Create labels
+const label = svg.selectAll(".label").data(graph.nodes).enter().append("text")
+  .attr("class", "label").text((d) => d.id)
+  .attr("text-anchor", "middle").attr("dy", ".35em")
+  .style("pointer-events", "none").style("user-select", "none")
+  .style("font-size", "16px").style("font-weight", "bold");
 
-//! Update positions on tick
+// Update positions on tick
 function ticked() {
   link
     .attr("x1", (d) => d.source.x)
@@ -233,7 +209,7 @@ function ticked() {
   label.attr("x", (d) => d.x).attr("y", (d) => d.y);
 }
 
-//! Drag behavior for nodes
+// Drag behavior for nodes
 function drag(simulation) {
   function dragStarted(event) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -259,7 +235,7 @@ function drag(simulation) {
     .on("end", dragEnded);
 }
 
-//! Algorithm selection
+// Algorithm selection
 const algorithmExplanations = {
   greedy:
     "The Greedy coloring algorithm is a simple and intuitive approach to vertex coloring. The algorithm iterates through the vertices of a graph, assigning the lowest available color to each vertex. The time complexity of this algorithm is O(n^2), where n is the number of vertices in the graph.",
@@ -277,13 +253,12 @@ function updateAlgorithmExplanation() {
   document.getElementById("algorithm-explanation").textContent = explanation;
 }
 
-// Update the algorithm explanation when the page is initially loaded
 updateAlgorithmExplanation();
 
 algorithmSelect.addEventListener("change", () => {
   const selectedAlgorithm = algorithmSelect.value;
   if (selectedAlgorithm === "greedy") {
-    coloringSteps = colorGraph(graph);
+    coloringSteps = colorGraphGreedy(graph);
   } else if (selectedAlgorithm === "dsatur") {
     coloringSteps = colorGraphDSatur(graph);
   } else if (selectedAlgorithm === "rlf") {
@@ -299,9 +274,9 @@ algorithmSelect.addEventListener("change", () => {
   updateAlgorithmExplanation();
 });
 
-let coloringSteps = colorGraph(graph);
+let coloringSteps = colorGraphGreedy(graph);
 
-//! Set initial node colors
+// Set initial node colors
 const uncoloredGraph = {};
 for (const node of graph.nodes) {
   uncoloredGraph[node.id] = null;
@@ -311,7 +286,7 @@ updateNodeColors(uncoloredGraph);
 // Set initial step index
 let currentStep = -1;
 
-//! Add event listeners for navigation buttons
+// Add event listeners for navigation buttons
 const backwardButton = document.getElementById("backward");
 const forwardButton = document.getElementById("forward");
 
@@ -333,7 +308,7 @@ forwardButton.addEventListener("click", () => {
   backwardButton.disabled = false;
 });
 
-//! Function to update node colors
+// Function to update node colors
 function updateNodeColors(colorMapping) {
   node.attr("fill", (d) =>
     colorMapping[d.id] ? colorScale(colorMapping[d.id]) : "lightgray"
